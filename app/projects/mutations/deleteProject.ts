dd46efa1e3daa6324c8b1a8032fd9daea4dac261
Gabriel Chertok <gchertok@gmail.com>
2021-07-22 12:32:54 -0300

    Use resolver.pipe

--- app/projects/mutations/deleteProject.ts
+++ app/projects/mutations/deleteProject.ts
@@ -1,18 +1,7 @@
-import { resolver, Ctx } from "blitz"
+import { resolver } from "blitz"
 import db from "db"
 
-// export default resolver.pipe(
-//   resolver.authorize(),
-//   resolver.zod(UpdateProject),
-//   async ({ id, name, color }) => {
-//     const project = await db.project.update({ data: { name, color }, where: { id } })
-//     return project
-//   }
-// )
-
-export default async function deleteProject({ id }: { id: number }, ctx: Ctx) {
-  ctx.session.$authorize()
-
+export default resolver.pipe(resolver.authorize(), async ({ id }) => {
   const project = await db.project.update({ data: { deletedAt: new Date() }, where: { id } })
   return project
-}
+})
--- app/projects/queries/getProjects.ts
+++ app/projects/queries/getProjects.ts
@@ -1,20 +1,11 @@
-import { resolver, Ctx } from "blitz"
+import { resolver } from "blitz"
 import db from "db"
 
-// export default resolver.pipe(resolver.authorize(), async () => {
-//   const projects = await db.project.findMany({
-//     orderBy: { createdAt: "asc" },
-//     where: { deletedAt: null },
-//   })
-
-//   return projects
-// })
-
-export default async function getProjects(_data, ctx: Ctx) {
-  ctx.session.$authorize()
+export default resolver.pipe(resolver.authorize(), async () => {
   const projects = await db.project.findMany({
     orderBy: { createdAt: "asc" },
     where: { deletedAt: null },
   })
+
   return projects
-}
+})
